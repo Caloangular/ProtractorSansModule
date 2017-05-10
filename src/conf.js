4a4a2d391dd1f836cbf358badc1758234b2362a1
Caloangular <caloangular@gmail.com>
2017-05-10 16:36:15 +0200

    Update conf.js

--- src/conf.js
+++ src/conf.js
@@ -2,7 +2,7 @@
 exports.config = {
    	directConnect: true,
 	framework: 'jasmine',
-	chromeDriver: '/home/jenkins/tools/com.cloudbees.jenkins.plugins.customtools.CustomTool/Chromedriver-2.6/chromedriver',
+	chromeDriver: 'node_modules/chromedriver/lib/chromedriver/chromedriver.exe',
 	specs: ['spec.js', 'spec2.js'],
 	capabilities: {
 		browserName: 'chrome'
